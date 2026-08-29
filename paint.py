9283fe41f1837d8d2f0cee2fed8d8ed6568bd28a
somevibecoder <christmastree4611@gmail.com>
2026-08-29 11:33:30 +0100

    Revise copyright and project description in paint.py
    Updated copyright information and modified project description.

--- paint.py
+++ paint.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
+# Copyright 2026 christmastree4611-dev
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://apache.org
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
-Mini Paint - a small Microsoft Paint clone.
-Pure Python + tkinter. No external dependencies.
-Runs on Windows / macOS / Linux.
+minipaint, a small mspaint
+python + tkinter. nothing else
+runs on windows / macOS / linux.
 
 Usage:
     python paint.py
+    py paint.py
 """
 
 import tkinter as tk
